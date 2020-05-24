e485d9a90c8e195551815067a5082ffeceeb0633
dream-dargon <330798125@qq.com>
2020-05-24 16:40:30 +0800

    '添加删除供应商数据'

--- two/src/pages/home/index.vue
+++ two/src/pages/home/index.vue
@@ -1,29 +1,65 @@
 <template>
   <div class="pages-home">
     <el-button @click="dialogVisible = true" type="primary">添加供应商</el-button>
-    <el-dialog title="添加" :visible.sync="dialogVisible" width="30%">
-      <span>这是一段信息</span>
+    <el-dialog :title='count' :visible.sync="dialogVisible" width="50%">
+      <span>
+        <el-form ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="供应商名称" prop="pass">
+            <el-input type="text" v-model="ruleForm.supplierName" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="联系人" prop="checkPass">
+            <el-input type="text" v-model="ruleForm.supplierContact" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="联系电话" prop="phone">
+            <el-input type="text" v-model.number="ruleForm.phone"></el-input>
+          </el-form-item>
+          <el-form-item label="地址" prop="address">
+            <el-input type="text" v-model.number="ruleForm.address"></el-input>
+          </el-form-item>
+          <el-form-item label="状态" prop="status">
+            <el-input type="text" v-model.number="ruleForm.status"></el-input>
+          </el-form-item>
+          <el-form-item label="描述" prop="description">
+            <el-input type="text" v-model.number="ruleForm.description"></el-input>
+          </el-form-item>
+        </el-form>
+      </span>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button @click="resetForm('ruleForm')">取 消</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')">确 定</el-button>
       </span>
     </el-dialog>
-    <el-table :data="list" border style="width: 100%">
-      <el-table-column prop="supplierName" label="供应商名称" width="180"></el-table-column>
-      <el-table-column prop="supplierContact" label="联系人" width="180"></el-table-column>
-      <el-table-column prop="phone" label="联系电话"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
-      <el-table-column prop="description" label="描述信息"></el-table-column>
-      <el-table-column label="操作">
-        <el-button type="primary">编辑</el-button>
-        <el-button type="danger">删除</el-button>
-      </el-table-column>
-    </el-table>
+    <table width="100%" border='1' cellSpacing='0' cellPadding="0">
+      <thead>
+          <tr>
+            <th>供应商名称</th>
+            <th>联系人</th>
+            <th>联系电话</th>
+            <th>地址</th>
+            <th>描述信息</th>
+            <th>操作</th>
+          </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(v) in list" :key='v.supplierId'>
+          <td>{{v.supplierName}}</td>
+          <td>{{v.supplierContact}}</td>
+          <td>{{v.phone}}</td>
+          <td>{{v.address}}</td>
+          <td>{{v.description}}</td>
+          <td>
+            <el-button type="primary" @click="editFn(v.supplierId)">编辑</el-button>
+            <el-button type="danger" @click="deleteFn(v.supplierId)">删除</el-button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+  import { mapState, mapMutations } from 'vuex'
+  import './style.less'
   export default {
     name: "Home",
     computed: {
@@ -32,7 +68,16 @@
 
     data() {
       return {
-        dialogVisible: false
+        dialogVisible: false,
+        count: '添加',
+        ruleForm: {
+          supplierName: '',
+          supplierContact: '',
+          phone: '',
+          address: '',
+          status: '',
+          description: ''
+        },
       };
     },
 
@@ -41,7 +86,31 @@
     },
 
     methods: {
-
+      submitForm(formName) {
+        this.dialogVisible = false;
+        console.log(this.ruleForm)
+        this['ADD_SUPPLIER_DATA'](this.ruleForm);
+        this.$refs[formName].validate((valid) => {
+          if (!valid) {
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.dialogVisible = false
+        this.$refs[formName].resetFields();
+      },
+      editFn (val) {
+        this.dialogVisible = true;
+        this.count = '编辑';
+        console.log(val)
+      },
+      deleteFn (val) {
+        let obj = {};
+        obj.id = val;
+        this['DELETE_SUPPLIER_DATA'](obj);
+      },
+      ...mapMutations(['ADD_SUPPLIER_DATA','DELETE_SUPPLIER_DATA'])
     }
   };
 </script>
--- two/src/store/index.js
+++ two/src/store/index.js
@@ -3,10 +3,12 @@ import Vuex from 'vuex'
 import { login } from './modules'
 import { 
   GET_SUPPLIER_LIST, 
-  SUPPLIER_LIST_ACTION 
+  SUPPLIER_LIST_ACTION,
+  ADD_SUPPLIER_DATA,
+  DELETE_SUPPLIER_DATA
 } from '@/constants/actionType'
 import api from '@/services/api'
-import { get } from '@/utils/request'
+import { get, post } from '@/utils/request'
 
 Vue.use(Vuex)
 
@@ -18,7 +20,24 @@ export default new Vuex.Store({
   mutations: {
     [GET_SUPPLIER_LIST] (state, action) {
       state.list = action;
+    },
+
+    [ADD_SUPPLIER_DATA] (state, action) {
+      post(api.addSupplier,action).then(res => {
+        if(res.code === 200) {
+          this.dispatch(SUPPLIER_LIST_ACTION);
+        }
+      })
+    },
+
+    [DELETE_SUPPLIER_DATA] (state, action) {
+      post(api.deleteSupplier,action).then(res => {
+        if(res.code === 200) {
+          this.dispatch(SUPPLIER_LIST_ACTION);
+        }
+      })
     }
+
   },
 
   actions: {
