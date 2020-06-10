56ba37fa26b7fee039fc2d8edc04ab14bc36cfef
dream-dargon <330798125@qq.com>
2020-06-10 16:23:08 +0800

    '删除第二次'

--- antd/src/App.js
+++ antd/src/App.js
@@ -2,10 +2,14 @@ import React from 'react'
 export default class App extends React.Component {
   render() {
     return (
+<<<<<<< HEAD
       <div>
          1111
          2222
       </div>
+=======
+      <div></div>
+>>>>>>> parent of ffd2260... '第二次'
     )
   }
 }
